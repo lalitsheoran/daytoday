147a4456c4d5704ddceaa100972a56ad8eeb67a0
lalitsheoran <lalit_sheoran@outlook.com>
2020-06-13 15:56:41 +0530

    update

--- hooks/todo/src/App.js
+++ hooks/todo/src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   const [task, setTask] = useState(null);
   const [addedTasks, setAddedTasks] = useState([]);
 
-  const handleIndividualTask = e => {
+  const handleIndividualTask = (e) => {
     if (e.keyCode === 13 && e.target.value.length !== 0) {
       handleAddTask();
     }
@@ -14,48 +14,83 @@ export default function App() {
   const handleAddTask = () => {
     setAddedTasks([
       ...addedTasks,
-      { taskId: uuidv4(), taskName: task, status: false }
+      { taskId: uuidv4(), taskName: task, status: false },
     ]);
     let inputTask = document.getElementById("inputTask");
     inputTask.value = null;
   };
-  const manageTask = e => {
-    let unique = e.target.getAttribute("unique");
+  const manageTask = (e) => {
+    let unique = e.target.parentElement.getAttribute("unique");
+    let relatedTo = e.target.getAttribute("related");
+    console.log(relatedTo, unique);
     let tasksCopy = [...addedTasks];
     if (unique) {
       console.log(unique, tasksCopy);
       for (let i = 0; i < tasksCopy.length; i++) {
-        if (tasksCopy[i].taskId === unique) {
+        if (tasksCopy[i].taskId === unique && relatedTo === "status") {
           tasksCopy[i].status = !tasksCopy[i].status;
           console.log("found and changes done");
+        } else if (tasksCopy[i].taskId === unique && relatedTo === "remove") {
+          tasksCopy = tasksCopy.filter((e) => e.taskId !== unique);
         }
       }
       setAddedTasks(tasksCopy);
     }
-    
   };
+  const iconAlert=()=>{
+    alert("clicked")
+  }
   return (
     <div>
       <input
         id="inputTask"
         onKeyDown={handleIndividualTask}
-        onChange={e => setTask(e.target.value)}
+        onChange={(e) => setTask(e.target.value)}
       />
       <button onClick={handleAddTask}>Add</button>
       <div>
         {addedTasks &&
-          addedTasks.map(e =>
-              <div
-                key={uuidv4()}
-                className={e.status ? "text-success" : "text-danger"}
-                unique={e.taskId}
-                onClick={manageTask}
-              >
-                {e.taskName}
+          addedTasks.map((e) =>
+            e.status ? (
+              <div key={uuidv4()} unique={e.taskId} className="d-flex" >
+                <p
+                  className="col-2"
+                  // className={e.status ? "text-success" : "text-danger"}
+                  related="status"
+                  onClick={manageTask}
+                >
+                  <del>{e.taskName}</del>
+                </p>
+                <span
+                  related="remove"
+                  onClick={iconAlert}
+                  className="iconify border"
+                  data-icon="clarity:remove-line"
+                  data-inline="false"
+                ></span>
               </div>
+            ) : (
+              <div key={uuidv4()} unique={e.taskId} className="d-flex">
+                <p
+                  className="col-2"
+                  // className={e.status ? "text-success" : "text-danger"}
+
+                  related="status"
+                  onClick={manageTask}
+                >
+                  {e.taskName}
+                </p>
+                <span
+                  related="remove"
+                  onClick={iconAlert}
+                  className="iconify border"
+                  data-icon="clarity:remove-line"
+                  data-inline="false"
+                ></span>
+              </div>
+            )
           )}
       </div>
-      <button>Show Completed Tasks</button>
     </div>
   );
 }
