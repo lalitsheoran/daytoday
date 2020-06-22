77d207892c85a15dc49f0ed1bd07e1fec906a488
lalitsheoran <lalit_sheoran@outlook.com>
2020-06-22 23:31:07 +0530

    update ll

--- ll/ll.js
+++ ll/ll.js
@@ -6,9 +6,19 @@ class Node{
 }
 class LinkedList{
     constructor(){
-        this.head=null,
-        this.tail=null,
-        this.length=0
+        this.head=null;
+        this.tail=null;
+        this.length=0;
+        
+    }
+    cleanUp(){
+        if(this.length===0){
+            this.head=null
+            this.tail=null
+        }
+        else if(this.length===1){
+            this.tail=null
+        }
     }
     push(value){
         let newNode= new Node(value)
@@ -34,12 +44,59 @@ class LinkedList{
             this.tail=prev
             this.tail.next=null
             this.length--
-            if(this.length===0){
-                this.head=null
-                this.tail=null
-            }
+            this.cleanUp()
             return current.value
         }
+    }
+    shift(){
+        if(!this.length){
+            return undefined
+        }
+        else{
+            let current = this.head
+            this.head=this.head.next
+            this.length--
+            this.cleanUp()
+            return current.value
+        }
+    }
+    unshift(value){
+        let newNode= new Node(value)
+        if(!this.head){
+            this.head=newNode
+        }
+        else{
+            let current=this.head
+            this.head=newNode
+            this.head.next=current
+        }
+        this.length++
+        return this
+
+    }
+    get(idx){
+        if(idx<0 || idx>=this.length){
+            return undefined
+        }
+        else{
+            let current= this.head
+            let count=0
+            while(count!=idx){
+                current=current.next
+                count++
+            }
+            return current
+        }
+        
+    }
+    set(idx,value){
+        let fvalue= this.get(idx)
+        if(fvalue){
+            fvalue.value=value
+            return true
+        }
+        return false
+        
     }
 }
 let ll=new LinkedList()
